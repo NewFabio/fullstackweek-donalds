9bff1155b3e8bf381f50b94713e8e13e3b10d493
NewFabio <fabio.conceicaoads@gmail.com>
2025-02-19 22:55:50 -0300

    chore: update app font

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,15 +1,14 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
-
 const ProductPage = () => {
-    return ( 
-        <div className="p-5 border border-red-500 rounded-xl">
-            <h1 className="text-red-500">Product Page</h1>
-            <Button>FSW 7.0</Button>
-            <Input placeholder="Teste componente"></Input>
-        </div>
-     );
-}
- 
+  return (
+    <div className="rounded-xl border border-red-500 p-5">
+      <h1 className="p-5 text-red-500">Product Page</h1>
+      <Button>FSW 7.0</Button>
+      <Input placeholder="Teste componente"></Input>
+    </div>
+  );
+};
+
 export default ProductPage;
