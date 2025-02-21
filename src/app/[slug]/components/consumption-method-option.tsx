accb5b186a41d7e7056c0cd25a29824a0d1b308b
NewFabio <fabio.conceicaoads@gmail.com>
2025-02-20 22:14:47 -0300

    feat: add restaurant menu page with categories and products

---
+++ src/app/[slug]/components/consumption-method-option.tsx
@@ -0,0 +1,41 @@
+import { ConsumptionMethod } from "@prisma/client";
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+
+interface ConsumptionMethodOptionsProps {
+  slug: string;
+  imageUrl: string;
+  imageAlt: string;
+  buttonText: string;
+  option: ConsumptionMethod;
+}
+const ConsumptionMethodOptions = ({
+  slug,
+  imageUrl,
+  imageAlt,
+  buttonText,
+  option,
+}: ConsumptionMethodOptionsProps) => {
+  return (
+    <Card>
+      <CardContent className="flex flex-col items-center gap-8 py-8">
+        <div className="relative h-[80px] w-[80px]">
+          <Image
+            src={imageUrl}
+            fill
+            alt={imageAlt}
+            className="object-contain"
+          />
+        </div>
+        <Button className="rounded-full" variant="secondary" asChild>
+          <Link href={`/${slug}/menu?consumptionMethod=${option}`}>{buttonText}</Link>
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ConsumptionMethodOptions;
--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,12 +1,58 @@
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+import { db } from "@/lib/prisma";
+
+import ConsumptionMethodOptions from "./components/consumption-method-option";
+
 interface RestaurantPageProps {
-    params: Promise<{slug: string}>
+  params: Promise<{ slug: string }>;
 }
 
-const RestaurantPage = async ({params}: RestaurantPageProps) => {
-    const {slug} = await params
-    return ( 
-        <h1>{slug}</h1>
-     );
-}
- 
+const RestaurantPage = async ({ params }: RestaurantPageProps) => {
+  const { slug } = await params;
+  const restaurant = await db.restaurant.findUnique({ where: { slug } });
+  if (!restaurant) {
+    return notFound();
+  }
+  return (
+    <div className="flex h-screen flex-col items-center justify-center px-6 pt-24">
+      {/* logo e titulo */}
+      <div className="flex flex-col items-center gap-2">
+        <Image
+          src={restaurant?.avatarImageUrl}
+          alt={restaurant.name}
+          width={82}
+          height={82}
+        />
+        <h2 className="font-semibold">{restaurant.name}</h2>
+      </div>
+      {/* seja bem vindo e escolha */}
+      <div className="space-y-2 pt-24 text-center">
+        <h3 className="text-2xl font-semibold">Seja Bem-vindo!</h3>
+        <p className="opacity-55">
+          Escolha como prefere aproveitar sua refeição. Estamos oferecendo
+          praticidade e sabor em cada detalhe!
+        </p>
+      </div>
+      <div className="grid grid-cols-2 gap-4 pt-14">
+        <ConsumptionMethodOptions
+          option="DINE_IN"
+          slug={slug}
+          buttonText="Para comer aqui"
+          imageAlt="Comer aqui"
+          imageUrl="/dine_in.png"
+        />
+        <ConsumptionMethodOptions
+          option="TAKEAWAY"
+          slug={slug}
+          buttonText="Para levar"
+          imageAlt="Para levar"
+          imageUrl="/takeaway.png"
+        />
+      </div>
+    </div>
+  );
+};
+
 export default RestaurantPage;
